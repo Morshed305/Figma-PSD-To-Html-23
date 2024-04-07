c8df54adc51481d58b29b85d525f27467bcaff6a
Morshed <morshedalam9691616@gmail.com>
2024-04-07 16:13:29 +0600

    Test commit

---
+++ 1st/Morshed Alam/js/Script.js
@@ -0,0 +1,15 @@
+// Nav
+const navTogger = document.querySelector(".nav-toggle");
+navTogger.addEventListener("click", navToggo);
+
+function navToggo(){
+    //  navTogger.classList.toggle("active");
+     const nav = document.querySelector("nav");
+     nav.classList.toggle("open");
+     if(nav.classList.contains("open") ){
+         nav.style.maxHeight = nav.scrollHeight + "px";
+     }else{
+         nav.removeAttribute("style")
+     }
+}
+// Nav End
